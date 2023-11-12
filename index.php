e833db054ad4c022a9d39bc231d53a9ddf3eda70
miccarroll <miccarroll9@gmail.com>
2023-11-11 21:09:03 -0500

    Adding basic HTML

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<h1>Hello World</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Form</title>
+</head>
+<body>
+  <h1>Form</h1>
+</body>
+</html>
