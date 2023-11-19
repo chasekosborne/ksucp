8b68b8bd12604272206034a70fa97c8c69416426
miccarroll <miccarroll9@gmail.com>
2023-11-19 11:47:27 -0500

    Slight style changes

--- index.php
+++ index.php
@@ -9,13 +9,13 @@
 
 <!-- Need to make more modern (of course) -->
 <body>
-  <div class="Titlebar">
-    <span class="Title">ksucp</span><span class="Subtitle"> | Kent State University Competitive Programming Club</span>
+  <div class="titlebar">
+    <span class="Title">ksucp</span><span class="subtitle"> | Kent State University Competitive Programming Club</span>
   </div>
   <!-- Still working on submission form -->
-  <div class = "Questionbox">
+  <div class = "questionbox">
     <form action="index.php" method="post">
-      University E-mail: <input type="text" name="email" autocomplete="off"><br>
+      University E-mail: <input type="text" name="email" autocomplete="off" placeholder="University E-mail"><br>
       <input id="submission" type="submit">
     </form>
   </div>
