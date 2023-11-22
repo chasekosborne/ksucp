01378c9541a80e2c9d9c192ab50720a2ac819edc
miccarroll <miccarroll9@gmail.com>
2023-11-21 19:36:00 -0500

    fixed bug

--- JS/form.js
+++ JS/form.js
@@ -5,7 +5,7 @@ jQuery(document).ready(($)=>{
   
 
   //hide all pages but page_1
-  $('form_page').css("display","none");
+  $('.form_page').css("display","none");
   $(".page_1").css("display","block");
 
 });
